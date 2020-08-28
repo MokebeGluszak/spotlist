ddec17bd3308271e2768d37b675f4fd5275fd086
Maciek Głuszak <maciek.gluszak@mcgluszak.com>
2020-08-28 11:10:29 +0200

    hbah

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
 import { Command, flags } from '@oclif/command'
+import { readFileSync } from "fs"
 
 class Spotlist extends Command {
   static description = 'kutas'
   static args = [{ name: 'schedule' }]
-
+  // D:\McGluszak Dropbox\Maciek Gluszak\Predykcje XP\source\ramuwki org\Polsat_cennik_wrzesien_2020.xlsx
   async run() {
     const { args } = this.parse(Spotlist)
-    console.log(args.schedule)
+
+    let msgOpen: string
+    let content: string
+    if (args.schedule !== undefined && args.schedule !== "") {
+      msgOpen = "otwieram plik: " + args.schedule
+       content = readFileSync (args.schedule , "utf8")
+    }
+    else {
+      msgOpen = "nie podałeś ścieżki chuju"
+      content = ""
+    }
+
+    console.log(msgOpen)
+    console.log (content)
     // const name = flags.name ?? 'world'
     // this.log(`hello ${name} from .\\src\\index.ts`)
     // if (args.file && flags.force) {
